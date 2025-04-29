7a82f9afb5d12be8b64c2dcbcf3f9cfc8688f12a
basic_anamay <anamay.brahme@gmail.com>
2025-04-29 15:37:30 +0200

    minor changes with deployment procedure

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import aws_cdk as cdk
 from drone_stack.drone_stack import DroneStack
 from ProcessingStack.processing_stack import ProcessingStack
 
-
 app = cdk.App()
 DroneStack(app, "DroneStack")
 ProcessingStack(app, "ProcessingStack")
